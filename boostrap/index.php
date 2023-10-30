beae4101d6034e49ee7e2e0a16ffcf739b94c9c1
karenplayboy <149425170+karenplayboy@users.noreply.github.com>
2023-10-30 13:44:00 -0500

    Add files via upload

---
+++ boostrap/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>boostrap 5 example</title>
+    <link href="bootstrap513/css/bootstrap.min.css" rel="stylesheet">
+    <script src="bootstrap513/js/bootstrap.bundle.min.js" ></script>
+</head>
+<body>
+    <div class="container-xxl text-center pt-5 bg-primary">
+    <h1>Hola mundo</h1> 
+</body>
+</html>
